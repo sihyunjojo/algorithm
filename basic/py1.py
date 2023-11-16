d4dae1db6a27e7853de24f336921d92c527467ff
sihyunjojo <si4018@naver.com>
2023-11-16 18:21:14 +0900

    feat: 전기버스 해결

---
+++ basic/py1.py
@@ -0,0 +1,23 @@
+a = [1, 2]
+# a.remove(2)
+print(a)
+
+
+newList = [i for i in a if a == [1, 2]]
+print(newList)
+newList = [i for i in a if i == 1]
+print(newList)
+print()
+
+n = 2
+arr = [0] + [int(input()) for _ in range(n)]
+print(arr)
+
+# nPr 서로 다른 것들 중 몇개를 뽑아서 하눚ㄹ로 나열하는것
+# nPr : n! / (n-r)!
+
+for i in range(2, 5):
+    print(i)
+
+
+
