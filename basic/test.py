642c0a5ea881afecb82a6efd9ded460984f7b970
sihyunjojo <si4018@naver.com>
2024-01-04 23:10:50 +0900

    test

--- basic/test.py
+++ basic/test.py
@@ -1,20 +1,49 @@
-r, c = map(int,input().split())
-arr = list(input() for _ in range(r))
+from collections import deque
 
+n = int(input())
+
+board = list(list(input()) for _ in range(n))
+v = [[False] * n for _ in range(n)]
+
+count,rg_count = 0,0
 delta = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-visited = [False for _ in range(26)]
-stack = [(0, 0)]
-walk = 0
 
-while stack:
-    x, y = stack.pop()
-    visited[ord(arr[x][y])-65] = True
+stack = deque()
 
-    for d in delta:
-        dx = d[0] + x
-        dy = d[1] + y
-        if 0 <= dx < r and 0 <= dy < c:
-            if not visited[ord(arr[dx][dy])-65]:
-                stack.append((dx, dy))
 
-print(visited.count(True))
+def bfs(y, x):
+    stack.append((y, x))
+    v[y][x] = True
+    while stack:
+        y, x = stack.popleft()
+        # v[y][x] = True
+
+        for d in delta:
+            dx = x + d[0]
+            dy = y + d[1]
+            if 0 <= dx < n and 0 <= dy < n:
+                if board[dy][dx] == board[y][x] and not v[dy][dx]:
+                    v[dy][dx] = True
+                    stack.append((dy, dx))
+
+
+for i in range(n):
+    for j in range(n):
+        if not v[i][j]:
+            bfs(i, j)
+            count += 1
+
+v = [[False] * n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 'R':
+            board[i][j] = 'G'
+
+for i in range(n):
+    for j in range(n):
+        if not v[i][j]:
+            bfs(i, j)
+            rg_count += 1
+
+print(count, rg_count)
