fcb6443ee85bc68b6383d9200986b0dfefdce068
sihyunjojo <si4018@naver.com>
2024-01-13 00:29:51 +0900

    feat: 해결

---
+++ backjoon/큐2.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+n = int(input())
+q = deque()
+
+for i in range(n):
+    order = input().split()
+    if order[0] == 'push':
+        q.append(order[1])
+    elif order[0] == 'pop':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif order[0] == 'size':
+        print(len(q))
+    elif order[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif order[0] == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
