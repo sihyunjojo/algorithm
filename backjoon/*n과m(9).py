937bd5fa89245662f477ded96df58e4ed2f9776f
sihyunjojo <si4018@naver.com>
2024-01-13 18:11:09 +0900

    feat: 해결
    불필요한 예외 처리는 성능을 느리게 만드므로, 중복 되는 예외처리나 중복되는 코드를 줄여야한다.
    생각생각생각

---
+++ backjoon/*n과m(9).py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+arr = []
+# visited = [False] * n
+arr1 = []
+dp = []
+def per(count):
+    if len(arr) == m:
+        dp.append(arr[:])
+        print(* arr)
+        return
+    for i in range(n):
+        if i not in arr1:
+            arr.append(nums[i])
+            arr1.append(i)
+            per(i)
+            arr.pop()
+            arr1.pop()
+            # visited[i] = False
+
+per(0)
+print(dp)
+dp = sorted(list(set(map(tuple,dp))))
+print(dp)
+
+for i in dp:
+    print(*i)
+
