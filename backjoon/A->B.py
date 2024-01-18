6cd4f81b3cfd1a11e095d0ef45a1859d3802cb44
sihyunjojo <si4018@naver.com>
2024-01-18 23:59:11 +0900

    feat: 해결 중

---
+++ backjoon/A->B.py
@@ -0,0 +1,26 @@
+a, b= map(int,input().split())
+dp = {b:-1}
+
+def go(n, count):
+    if n > b:
+        return
+    dp[n] = count
+    if list(dp.keys()).count(n) == 1:
+        dp[n] = min(dp[n], count)
+
+    go(int(str(n)+"1"), count+1)
+    go(n * 2, count+1)
+
+go(a,1)
+
+
+# print(dp)
+
+# print(dp)
+# print(list(dp.keys()).count(21));
+print(dp[b])
+
+
+
+
+
