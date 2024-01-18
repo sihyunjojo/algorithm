d4c394b5c401dbdb17c50d12cc7c0cfba4a6ff6f
sihyunjojo <si4018@naver.com>
2024-01-18 23:59:57 +0900

    feat: 해결

--- backjoon/A->B.py
+++ backjoon/A->B.py
@@ -5,8 +5,8 @@ def go(n, count):
     if n > b:
         return
     dp[n] = count
-    if list(dp.keys()).count(n) == 1:
-        dp[n] = min(dp[n], count)
+    # if list(dp.keys()).count(n) == 1:
+    #     dp[n] = min(dp[n], count)
 
     go(int(str(n)+"1"), count+1)
     go(n * 2, count+1)
